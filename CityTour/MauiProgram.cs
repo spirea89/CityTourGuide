1519f3bb8eedc940fbd17d7d9f8a154f77f842e8
spirea89 <42568080+spirea89@users.noreply.github.com>
2025-09-18 15:15:41 +0200

    Enable scroll wheel zoom on map

--- CityTour/MauiProgram.cs
+++ CityTour/MauiProgram.cs
@@ -1,6 +1,12 @@
+using System;
 using System.Net.Http;
 using CityTour.Services;
 using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Handlers;
+#if WINDOWS
+using Microsoft.UI.Xaml.Controls.Maps;
+using Microsoft.UI.Xaml.Input;
+#endif
 
 namespace CityTour;
 
@@ -24,6 +30,39 @@ public static class MauiProgram
         builder.Services.AddSingleton<MainPage>();
         builder.Services.AddTransient<Views.DetailPage>();
 
+#if WINDOWS
+        MapHandler.Mapper.AppendToMapping("ScrollWheelZoom", (handler, view) =>
+        {
+            if (handler.PlatformView is MapControl map)
+            {
+                map.PointerWheelChanged -= OnMapPointerWheelChanged;
+                map.PointerWheelChanged += OnMapPointerWheelChanged;
+            }
+        });
+#endif
+
         return builder.Build();
     }
+
+#if WINDOWS
+    private static void OnMapPointerWheelChanged(object sender, PointerRoutedEventArgs e)
+    {
+        if (sender is not MapControl map)
+            return;
+
+        var delta = e.GetCurrentPoint(map).Properties.MouseWheelDelta;
+        if (delta == 0)
+            return;
+
+        var zoomChange = delta > 0 ? 0.5 : -0.5;
+        var targetZoom = Math.Clamp(map.ZoomLevel + zoomChange, map.MinZoomLevel, map.MaxZoomLevel);
+
+        if (Math.Abs(targetZoom - map.ZoomLevel) > double.Epsilon)
+        {
+            map.ZoomLevel = targetZoom;
+        }
+
+        e.Handled = true;
+    }
+#endif
 }
